e212404ab34736b3074a8568918a17975203f39d
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-06-04 21:07:04 +0545

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from './Components/Home/Navbar/Navbar';
-import Body from './Components/Home/Body/body';
-import BookingForm from './Components/Home/BookingForm/BookingForm';
-import ImageHolder from './Components/Home/ImageHolder/ImageHolder';
-import Footer from './Components/Home/Footer/Footer';
-import About from './Components/About/About';
-import Services from './Components/Services/Services';
-import Contact from './Components/Contact/Contact';
+import Navbar from "./Components/Home/Navbar/Navbar";
+import Body from "./Components/Home/Body/body";
+import BookingForm from "./Components/Home/BookingForm/BookingForm";
+import ImageHolder from "./Components/Home/ImageHolder/ImageHolder";
+import Footer from "./Components/Home/Footer/Footer";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import Contact from "./Components/Contact/Contact";
 
 // Create a Layout component that includes the Navbar and Footer
 const Layout = ({ children }) => (
   <>
     <Navbar />
-    <main className="main-content">
-      {children}
-    </main>
+    <main className="main-content">{children}</main>
     <Footer />
   </>
 );
@@ -26,8 +24,9 @@ const Layout = ({ children }) => (
 const Home = () => (
   <>
     <Body />
-    <BookingForm />
     <ImageHolder />
+  
+    <BookingForm />
   </>
 );
 
@@ -35,16 +34,44 @@ const AboutPage = () => <About />;
 
 const ServicesPage = () => <Services />;
 
-const ContactPage = () => <Contact/>;
+const ContactPage = () => <Contact />;
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/about" element={<Layout><AboutPage /></Layout>} />
-        <Route path="/services" element={<Layout><ServicesPage /></Layout>} />
-        <Route path="/Contact" element={<Layout><ContactPage/></Layout>}/>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <AboutPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <Layout>
+              <ServicesPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/Contact"
+          element={
+            <Layout>
+              <ContactPage />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
